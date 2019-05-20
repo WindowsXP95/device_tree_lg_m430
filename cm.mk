ec001459637f32372410b992e7c5dce4c4744132
WindowsXP95 <windowsxp519@gmail.com>
2019-05-20 09:37:00 -0400

    Update and rename cm.mk.template to cm.mk

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := narnia
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Leapfrog/Epic/epic.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Epic
+PRODUCT_NAME := cm_narnia
+PRODUCT_BRAND := Leapfrog Epic
+PRODUCT_MODEL := narnia
+PRODUCT_MANUFACTURER := Leapfrog
